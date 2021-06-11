7285f4e83b5e25c90dab92e7c1e35070dd430495
Askar Rizvi <askarrizvi@askars-mbp.lan>
2021-06-11 15:19:13 -0400

    Implement function to get the different character types

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,34 @@
 //Define generatePassword function here
 function generatePassword(){
   var length = getLength();
-  //var lowerCase = window.
-  return length;
+  var types = getTypes();
 }
 
 //Function to get the length and make sure user entered a valid value
 function getLength(){
   var gLength = window.prompt("How long do you want the password to be?(can be between 8 to 128 characters)");
-  debugger;
+  //Check to see if the value entered is valid
   if (isNaN(gLength)||gLength>120||gLength<8){
     window.alert("Please enter a valid value");
     gLength = getLength();
   } 
   return gLength;
+}
+
+//Function to get the different types of characters
+function getTypes(){
+  var lowerCase = window.confirm("Do you want to include lowercase characters?");
+  var upperCase = window.confirm("Do you want to include uppercase characters?");
+  var numeric = window.confirm("Do you want to include numeric characters?");
+  var specialChars = window.confirm("Do you want to include special characters?");
+  //Store all of the answers in an array
+  var typesArray = [lowerCase, upperCase, numeric, specialChars];
+  //Check to see to make sure that there is atleast one true type
+  if (!typesArray.includes(true)){
+    window.alert("You must include atleast one type of character")
+    typesArray = getTypes();
+  }
+  return typesArray;
 }
 
 // Get references to the #generate element
